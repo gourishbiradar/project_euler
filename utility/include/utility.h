6cb497c6c855a8a99cc79c3b126cf7ee298ad5b6
Gourish K Biradar <gourishk.biradar@citrix.com>
2020-08-05 15:48:25 +0530

    Making isPlaindrome non template

--- utility/include/utility.h
+++ utility/include/utility.h
@@ -20,8 +20,7 @@ namespace numberUtils
 {
 	std::vector<long int> getPrimes(double limit);
 	std::vector<long int> getPrimeDivisors(long int number);
-	template<typename T>
-		bool isPalindrome(T number);
+	bool isPalindrome(int number);
 }
 
 namespace VectorUtils 
