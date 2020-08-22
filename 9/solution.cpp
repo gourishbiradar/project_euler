95ce2d4abc873700d1bdf3186aca0317f2119768
Gourish K Biradar <gourishk.biradar@citrix.com>
2020-08-22 21:42:27 +0530

    Implemented Dickson's method

--- 9/solution.cpp
+++ 9/solution.cpp
@@ -4,7 +4,28 @@
 #include <sstream>
 #include "utility.h"
 
-
+unsigned long long int findTriplet(unsigned long long int range)
+{
+	for(unsigned long long int r = 1; r<=range;r++)
+	{
+		unsigned long long int r_sqr_by_2 = (unsigned long long int)(pow(r,2)/2);
+		for(unsigned long long int s = 1; s<r;s++)
+		{
+			if((r_sqr_by_2)%s == 0)
+			{
+				unsigned long long int t = (r_sqr_by_2)/s;
+				unsigned long long int a,b,c;
+				a = r+s;
+				b = r+t;
+				c = r+s+t;
+				if(a+b+c == range)
+				{
+					return a*b*c;
+				}
+			}
+		}
+	}
+}
 int main(int argc, char** argv)
 {
 	Timer t("Project Euler problem 9");
@@ -16,25 +37,6 @@ int main(int argc, char** argv)
 	std::string range_str(argv[1]);
 	unsigned long long int range = stoll(range_str);
 	std::cout << "Input number : " << range << "\n" ;
-	unsigned long long int answer = 0;
-	for(unsigned long long int a = 1 ; a < range ; a++)
-	{
-		for(unsigned long long int b = a+1; b < range ; b++)
-		{
-			unsigned long long int c = range - (a+b); //condition is a+b+c = range
-			if(pow(a,2) + pow(b,2) == pow(c,2))
-			{
-				std::cout << "Found a triplet (a,b,c) " << a << " " << b << " " << c << "\n";
-				answer = a*b*c;
-				break;
-			}
-		}
-	}
-	if(answer == 0 )
-	{
-		std::cout << "Could not find any such triplet\n";
-		return -2;
-	}
-	std::cout << "Product is : " << answer << "\n";
+	std::cout << "Product is : " << findTriplet(range) << "\n";
 	return 0;
 }
